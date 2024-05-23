24c5d23e295c6ad69872ed4bd8415f6739161de3
ChorchPig <96993431+ChorchPig@users.noreply.github.com>
2024-05-23 17:49:40 -0300

    Add files via upload

--- TDAGrafo/main.c
+++ TDAGrafo/main.c
@@ -2,45 +2,55 @@
 #include <stdlib.h>
 #include "prototipos.h"
 
+binTree** binTreeFind3(binTree**,t_elem_btree); //Acomoda los nodos para que los más buscados estén más cerca de la raíz
+void binTreeMirrorInsert(binTree**,binTree*,int());
+int restarDosEnteros2(int,int);
+
 int main(){
-    Grafo *grafo=crearGrafo(5);
-    crearVertice(grafo, 'a');
-    crearVertice(grafo, 'b');
-    crearVertice(grafo, 'c');
-    crearVertice(grafo, 'd');
-    crearVertice(grafo, 'e');
-    crearArista(grafo, 'a', 'c');
-    crearArista(grafo, 'a', 'd');
-    crearArista(grafo, 'b', 'd');
-    crearArista(grafo, 'b', 'e');
-    crearArista(grafo, 'a', 'c');
-    crearArista(grafo, 'e', 'c');
-    matrix *aristas=obtenerAristas(grafo);
-    imprimirMatriz(aristas);
+    binTree *arbol=NULL, *arbolEspejado=NULL;
+    btn_insert_value(&arbol, 10, restarDosEnteros);
+    btn_insert_value(&arbol, 15, restarDosEnteros);
+    btn_insert_value(&arbol, 5, restarDosEnteros);
+    btn_insert_value(&arbol, 1, restarDosEnteros);
+    btn_insert_value(&arbol, 9, restarDosEnteros);
+    btn_insert_value(&arbol, 11, restarDosEnteros);
+    btn_insert_value(&arbol, 20, restarDosEnteros);
+    btn_insert_value(&arbol, 16, restarDosEnteros);
+    btn_insert_value(&arbolEspejado, 10, restarDosEnteros2);
+    btn_insert_value(&arbolEspejado, 15, restarDosEnteros2);
+    btn_insert_value(&arbolEspejado, 5, restarDosEnteros2);
+    btn_insert_value(&arbolEspejado, 1, restarDosEnteros2);
+    btn_insert_value(&arbolEspejado, 9, restarDosEnteros2);
+    btn_insert_value(&arbolEspejado, 11, restarDosEnteros2);
+    btn_insert_value(&arbolEspejado, 20, restarDosEnteros2);
+    btn_insert_value(&arbolEspejado, 16, restarDosEnteros2);
+    char formato='i';
+    recorridoAmplitud(arbol, imprimirValor, &formato);
     printf("\n");
-    int grado=obtenerGradoVertice(grafo, 0);
-    printf("%d\n", grado);
-    eliminarGrafo(grafo);
+    //binTreeMirrorInsert(arbol, arbolEspejado, restarDosEnteros2);
+    recorridoAmplitud(arbolEspejado, imprimirValor, &formato);
+    printf("\n");
+    eliminarArbol(&arbol, restarDosEnteros);
     return 0;
 }
 
-matrix* ampliarMatriz(matrix *Matrix, int filas, int columnas){
-    if(!Matrix)return Matrix;
-    matrix *nuevaMatriz=crearMatriz(filas, columnas);
-    if(nuevaMatriz){
-        int filasViejas=getMatrixRows(Matrix), columnasViejas=getMatrixColumns(Matrix);
-        for(int i=0; i<filas; i++){
-            for(int j=0; j<columnas; j++){
-                if(i<filasViejas&&j<columnasViejas){
-                    setValueInMatrix(nuevaMatriz, getValueInMatrix(Matrix, i, j), i, j);
-                }
-            }
-        }
-        free(Matrix);
+void recorridoAmplitud(binTree *arbol, void btn_do(binTree*,void*), void *contexto){
+    nodo *aux=NULL;
+    Fila *fila=crearFila(-1);
+    enqueue(fila, arbol);
+    while(!filaVacia(fila)){
+        aux=dequeue(fila);
+        btn_do(aux->nodeValue, contexto);
+        if(aux->nodeValue->left)enqueue(fila, aux->nodeValue->left);
+        if(aux->nodeValue->right)enqueue(fila, aux->nodeValue->right);
     }
-    return nuevaMatriz;
+    eliminarFila(fila);
 }
 
-int obtenerCantVertices(Grafo *grafo){
-    return grafo->vertices->currentSize;
+void eliminarFila(Fila *fila){
+    if(!fila)return;
+    while(!filaVacia(fila))dequeueFree(fila);
+    free(fila); //por alguna razón se traba acá
 }
+
+int restarDosEnteros2(int a, int b) { return b-a; }
